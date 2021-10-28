0c8d8750ecbc93ec248f239ac19a783208daea60
viena maulina <vienamaulina@gmail.com>
2021-10-28 20:28:38 +0700

    menambahkan file python

---
+++ Latihan1.py
@@ -0,0 +1,17 @@
+print("Biodata Diri")
+
+Name=input("Name:")
+Call=input("Call:")
+Nim=input("Nim:")
+Age=input("Age:")
+Dob=input("Dob:")
+Address=input("Address:")
+Handphone=input("Handphone:")
+
+
+print("assalamualaikum")
+print("let me introduce my self. my name is",Name,"but you can call me",Call,
+"my NIM is",Nim,"i was born in",Dob,"and i am",Age,
+"years old. i am very glad if you want to invite my house in",Address,
+".so, dont forget to call me before with the handphone",Handphone,"")
+print("thank you")
